df272861a1544959cdfb08fa44b4fa1360778490
vikzak <victor@azbukait.ru>
2021-12-05 06:06:16 +0300

    ДЗ№4 и частично ДЗ№5 + исправлены замечания с ДЗ№3

--- app/src/main/java/com/example/android1lesson02_2/MainActivity.java
+++ app/src/main/java/com/example/android1lesson02_2/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.android1lesson02_2;
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContract;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,15 +13,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static final String INPUT_ARG = "INPUT_ARG"; // аргумент для передачи
-    private TextView Screen;
+    private TextView screenLED;
     private Button button0, button1, button2, button3, button4, button5, button6, button7, button8, button9;
-    private Button buttonSum, buttonDiff, buttonDiv, buttonMul, buttonDot, buttonSwitch, buttonC,buttonCE, buttonSq, buttonItog;
+    private Button buttonSum, buttonDiff, buttonDiv, buttonMul, buttonDot, buttonSwitch, buttonC, buttonCE, buttonSq, buttonItog;
     // кнопки:       +            -          /           *          .           +/-             C     CE       корень      =
-    private String input,Answer;
+    private String input, Answer;
     private String lastKeyOperation;
     private static final String ARG_SAVED_THEME = "ARG_SAVED_THEME";
     private int currrentTheme = R.style.Theme_Android1Lesson022_Second; //4
@@ -31,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private final ActivityResultLauncher<Intent> launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
         @Override
         public void onActivityResult(ActivityResult result) {
-            if (result.getResultCode() == Activity.RESULT_OK){
+            if (result.getResultCode() == Activity.RESULT_OK) {
                 Theme theme = (Theme) result.getData().getSerializableExtra(SelectThemeActivity.EXTRA_THEME);
                 storage.saveTheme(theme);
                 recreate();
@@ -50,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
         storage = new ThemeStorage(this);
 
-        if (savedInstanceState != null){
+        if (savedInstanceState != null) {
             currrentTheme = savedInstanceState.getInt(ARG_SAVED_THEME);
         }
         //setTheme(currrentTheme); // передаем 2ю тему
@@ -58,11 +56,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // заранее определяем Screen, чтобы передать в него при повороте экрана данные
-        Screen = findViewById(R.id.textViewLED);
+        screenLED = findViewById(R.id.textViewLED);
         // проверка savedInstanceState
-        if (savedInstanceState != null){
+        if (savedInstanceState != null) {
             input = savedInstanceState.getString(INPUT_ARG);
-            Screen.setText(input);
+            screenLED.setText(input);
         }
 
         // - функциональные кнопки
@@ -91,12 +89,11 @@ public class MainActivity extends AppCompatActivity {
         button9 = findViewById(R.id.button9);
 
 
-
         findViewById(R.id.buttonSwitch).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, SelectThemeActivity.class);
-                intent.putExtra(SelectThemeActivity.EXTRA_THEME,storage.getSavedTheme());
+                intent.putExtra(SelectThemeActivity.EXTRA_THEME, storage.getSavedTheme());
                 launcher.launch(intent);
             }
         });
@@ -106,11 +103,11 @@ public class MainActivity extends AppCompatActivity {
 //        }
     }
 
-    public void buttonClick(View view){
+    public void buttonClick(View view) {
         Button button = (Button) view;
         String data = button.getText().toString();
         //lastKeyOperation = "";
-        switch (data){
+        switch (data) {
             case "C":
                 input = "";
                 break;
@@ -127,10 +124,10 @@ public class MainActivity extends AppCompatActivity {
                 Answer = input;
                 break;
             case "CE":
-                if (input != null){ // если на экране не пусто
-                    if (input.length() > 0){
-                        String newText = input.substring(0,input.length()-1);
-                        if (newText.length() == 0){
+                if (input != null) { // если на экране не пусто
+                    if (input.length() > 0) {
+                        String newText = input.substring(0, input.length() - 1);
+                        if (newText.length() == 0) {
                             newText = "";
                         }
                         input = newText;
@@ -143,94 +140,84 @@ public class MainActivity extends AppCompatActivity {
                 input = "";
                 break;
             default:
-                if (input == null){
+                if (input == null) {
                     input = "";
                 }
-                if (data.equals("+") || data.equals("-") || data.equals("/")){
+                if (data.equals("+") || data.equals("-") || data.equals("/")) {
                     //statusKeyOperation = true;
                     calculateMetod();
                 }
-                input+=data;
+                input += data;
         }
-        Screen.setText(input);
+        screenLED.setText(input);
 
     }
 
-    private void calculateMetod(){
-        if (input.split("\\*").length == 2){
+    private void calculateMetod() {
+        if (input.split("\\*").length == 2) {
             String number[] = input.split("\\*");
             try {
                 double mul = Double.parseDouble(number[0]) * Double.parseDouble(number[1]);
                 input = mul + "";
-            }
-            catch (Exception e){
+            } catch (Exception e) {
                 // error
             }
-        }
-        else if (input.split("/").length == 2){
+        } else if (input.split("/").length == 2) {
             String number[] = input.split("/");
             try {
-                if (Double.parseDouble(number[1])!=0)
-                {
+                if (Double.parseDouble(number[1]) != 0) {
                     double div = Double.parseDouble(number[0]) / Double.parseDouble(number[1]);
-                    input =div + "";
+                    input = div + "";
                 } else {
                     //("Деление на 0 - ошибка!") - просто перезаписываем пустотой
                     input = "";
                 }
 
-            }
-            catch (Exception e){
+            } catch (Exception e) {
                 // error
             }
-        }
-        else if (input.split("\\+").length == 2){
+        } else if (input.split("\\+").length == 2) {
             String number[] = input.split("\\+");
             try {
                 double sum = Double.parseDouble(number[0]) + Double.parseDouble(number[1]);
-                input = sum +"";
-            }
-            catch (Exception e){
+                input = sum + "";
+            } catch (Exception e) {
                 //error
             }
-        }
-
-        else if (input.split("-").length > 1) {
+        } else if (input.split("-").length > 1) {
             String number[] = input.split("-");
-            if (number[0] == "" && number.length == 2){
-                number[0] = 0 +"";
+            if (number[0] == "" && number.length == 2) {
+                number[0] = 0 + "";
             }
             try {
                 double sub = 0;
-                if (number.length == 2){
+                if (number.length == 2) {
                     sub = Double.parseDouble(number[0]) - Double.parseDouble(number[1]);
                 } else if (number.length == 3) {
                     sub = -Double.parseDouble(number[1]) - Double.parseDouble(number[2]);
                 }
                 input = sub + "";
-            }
-            catch (Exception e){
+            } catch (Exception e) {
                 // error
             }
         }
         // удаляем 0 из целочисленного результата
         String n[] = input.split("\\.");
-        if (n.length > 1){
-            if (n[1].equals("0")){
+        if (n.length > 1) {
+            if (n[1].equals("0")) {
                 input = n[0];
             }
         }
-        Screen.setText(input);
-        lastKeyOperation="";
+        screenLED.setText(input);
+        lastKeyOperation = "";
     }
 
     @Override
-    public void onSaveInstanceState(@NonNull Bundle instanceState){
+    public void onSaveInstanceState(@NonNull Bundle instanceState) {
         super.onSaveInstanceState(instanceState);
         instanceState.putString(INPUT_ARG, input); //1
         instanceState.putInt(ARG_SAVED_THEME, currrentTheme);
     }
-
 
 
     // остальные методы на момент задания №3 не используются, комментируем их
